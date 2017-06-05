f3ade558659a830de3090be3f0bfda9623b158e8
Katherine Balmeo <kpbalmeo@gmail.com>
2017-06-06 02:08:39 +0800

    added delete code feature

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -2,4 +2,10 @@ class CodesController < ApplicationController
   def index
   	@codes = Code.all
   end
+
+  def destroy
+  	@code = Code.find(params[:id])
+ 	@code.destroy
+ 	redirect_to codes_path
+  end
 end
