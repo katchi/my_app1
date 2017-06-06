484ef262e3032fbf116da299a69504387693344e
Katherine Balmeo <kpbalmeo@gmail.com>
2017-06-06 09:52:32 +0800

    added add code feature

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -8,4 +8,15 @@ class CodesController < ApplicationController
  	@code.destroy
  	redirect_to codes_path
   end
+
+  def new
+  	@code = Code.new
+  end
+
+  def create
+  	@code = Code.new(coder_id: params[:code][:coder_id], content: params[:code][:content], language: params[:code][:language])
+  	@code.save
+  	redirect_to codes_path
+  end
+
 end
