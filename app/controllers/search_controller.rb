31a3e0d1a1115d3a70d63c80d09d8b80f265046b
Katherine Balmeo <kpbalmeo@gmail.com>
2017-06-09 19:25:23 +0800

    refactored search feature view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,6 @@ class SearchController < ApplicationController
 	def index
 		@codes = Code.where("content ILIKE ?", "%#{params[:q]}%")
 		@coders = Coder.where("firstname ILIKE ?", "%#{params[:q]}%")
+		@results = @codes + @coders
 	end
 end	
