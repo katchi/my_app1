a7a02106566bdd9fc6b7636331207fd92d971302
Katherine Balmeo <kpbalmeo@gmail.com>
2017-06-12 21:51:25 +0800

    generate migration file for adding description field in table codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605160850) do
+ActiveRecord::Schema.define(version: 20170612134943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20170605160850) do
     t.text     "content"
     t.string   "language"
     t.boolean  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
